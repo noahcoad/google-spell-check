1cc385c78995199431e2dcc11ef4ddcc1c19f5a3
Noah Coad <noah@coad.net>
2013-07-04 12:07:19 -0500

    removed debugging lines

--- google-spell-check.py
+++ google-spell-check.py
@@ -18,11 +18,11 @@ class GoogleSpellCheckCommand(sublime_plugin.TextCommand):
 		# grab html
 		html = self.get_page('http://www.google.com/search?q=' + urllib2.quote(text))
 
-		open('page.html', 'w').write(html)
+		# save html for debugging
+		# open('page.html', 'w').write(html)
 
 		# pull pieces out
 		match = re.search(r'(?:Showing results for|Did you mean|Including results for)[^\0]*?<a.*?>(.*?)</a>', html)
-		print(match)
 		if match is None:
 			fix = text
 		else:
@@ -36,7 +36,7 @@ class GoogleSpellCheckCommand(sublime_plugin.TextCommand):
 		# the type of header affects the type of response google returns
 		# for example, using the commented out header below google does not 
 		# include "Including results for" results and gives back a different set of results
-		# than using the updated user_agent pulled from chrome
+		# than using the updated user_agent yanked from chrome's headers
 		# user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 		user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'
 		headers = {'User-Agent':user_agent,}
