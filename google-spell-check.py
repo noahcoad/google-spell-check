d23d5dc6dc439155e69cb91d2c097b77655929a0
conradbez <conradbez1.com>
2016-08-05 16:06:13 +0800

    fixes google new syntax which sometimes causes return of knowlege box as suggested words

--- google-spell-check.py
+++ google-spell-check.py
@@ -21,20 +21,28 @@ class GoogleSpellCheckCommand(sublime_plugin.TextCommand):
 		# grab html
 		html_result = self.get_page('http://www.google.com/search?hl=en&q=' + urllib.parse.quote(text))
 		html_parser = html.parser.HTMLParser()
+		print(html_result)
 
-		# save html for debugging
-		# open('page.html', 'w').write(html)
+		start_google_suggestion = html_result.find('<a class="spell" href=')
 
-		# pull pieces out
-		match = re.search(r'(?:Showing results for|Did you mean|Including results for)[^\0]*?<a.*?><b.*?><i.*?>(.*?)</i>', html_result)
+		# test if result was found
+		if start_google_suggestion==-1:
+			return text
+		else:
+			# finds dimesnions of google duggestion
+			end_google_sug = html_result.find('</a>',start_google_suggestion)
+
+			# finds dimesnions of suggested text
+			start_sug_text = html_result.find("&amp;q=",start_google_suggestion,end_google_sug)+7
+			end_sug_text = html_result.find("&amp;spell",start_sug_text,end_google_sug)
+			sug_text = html_result[start_sug_text:end_sug_text]
+			match = urllib.parse.unquote(sug_text.replace('+',' '))
+
 		if match is None:
+			print('no match')
 			fix = text
 		else:
-			fix = match.group(1)
-			fix = re.sub(r'<.*?>', '', fix)
-			fix = html_parser.unescape(fix)
-
-		# return result
+			fix = match
 		return fix
 
 	def get_page(self, url):
@@ -43,7 +51,7 @@ class GoogleSpellCheckCommand(sublime_plugin.TextCommand):
 		# include "Including results for" results and gives back a different set of results
 		# than using the updated user_agent yanked from chrome's headers
 		# user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-		user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'
+		user_agent ='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 		headers = {'User-Agent':user_agent,}
 		req = urllib.request.Request(url, None, headers)
 		page = urllib.request.urlopen(req)
