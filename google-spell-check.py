c87f7c90458cccf636a98e6992125fa1512e9ea2
conradbez <conradbez1.com>
2016-08-05 15:55:18 +0800

    fixes; google new syntax

--- google-spell-check.py
+++ google-spell-check.py
@@ -1,7 +1,8 @@
 # Replaces the word under the cursor or selection with Google Search's recommended spelling
 # Hosted at http://github.com/noahcoad/google-spell-check
+# miscellaneous jon sculzi bookz
 
-import sublime, sublime_plugin, urllib2, re, HTMLParser
+import sublime, sublime_plugin, urllib.request, urllib.parse, re, html.parser
 
 class GoogleSpellCheckCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -13,20 +14,19 @@ class GoogleSpellCheckCommand(sublime_plugin.TextCommand):
 				continue
 
 			fix = self.correct(self.view.substr(sel))
-			edit = self.view.begin_edit()
 			self.view.replace(edit, sel, fix)
 			self.view.end_edit(edit)
 
 	def correct(self, text):
 		# grab html
-		html = self.get_page('http://www.google.com/search?hl=en&q=' + urllib2.quote(text) + "&meta=&gws_rd=ssl")
-		html_parser = HTMLParser.HTMLParser()
+		html_result = self.get_page('http://www.google.com/search?hl=en&q=' + urllib.parse.quote(text))
+		html_parser = html.parser.HTMLParser()
 
 		# save html for debugging
 		# open('page.html', 'w').write(html)
 
 		# pull pieces out
-		match = re.search(r'(?:Showing results for|Did you mean|Including results for)[^\0]*?<a.*?>(.*?)</a>', html)
+		match = re.search(r'(?:Showing results for|Did you mean|Including results for)[^\0]*?<a.*?><b.*?><i.*?>(.*?)</i>', html_result)
 		if match is None:
 			fix = text
 		else:
@@ -45,8 +45,8 @@ class GoogleSpellCheckCommand(sublime_plugin.TextCommand):
 		# user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 		user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'
 		headers = {'User-Agent':user_agent,}
-		req = urllib2.Request(url, None, headers)
-		page = urllib2.urlopen(req)
+		req = urllib.request.Request(url, None, headers)
+		page = urllib.request.urlopen(req)
 		html = str(page.read())
 		page.close()
 		return html
